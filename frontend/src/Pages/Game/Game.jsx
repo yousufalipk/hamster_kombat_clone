4d764db8eb78b2ad7f8474918c681a092d8d61e0
ameeryousufalibhatti <yousufbhatti2002@gmail.com>
2024-09-23 10:54:52 +0500

    Changes

--- frontend/src/Pages/Game/Game.jsx
+++ frontend/src/Pages/Game/Game.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const GamePlay = () => {
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const tg = window.Telegram.WebApp;
+
+		tg.BackButton.show();
+		tg.BackButton.onClick(() => {
+			navigate("/");
+		});
+
+		return () => {
+			tg.BackButton.hide();
+			tg.BackButton.offClick();
+		};
+	}, [navigate]);
+
     return (
         <>
 			<div className='h-[100vh] bg-custom-image3 bg-cover flex justify-center items-center overflow-hidden'>
