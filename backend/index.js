435fc749cbfe9c7e74aecd753da98f58baae9348
ameeryousufalibhatti <yousufbhatti2002@gmail.com>
2024-10-15 12:58:05 +0500

    Changes

--- backend/index.js
+++ backend/index.js
@@ -2,32 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const { PORT, FRONTEND_APP_PATH } = require('./config/env');
 const connectToDb = require('./config/db');
-const user = require('./routes/userRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
+// Connect to the database
 connectToDb();
 
+// Middleware
 app.use(express.json());
 
-
+// CORS configuration
 const corsOptions = {
-    origin: FRONTEND_ORIGIN,
+    origin: FRONTEND_APP_PATH,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 app.use(cors(corsOptions));
 
+// Define a root route for testing
 app.get('/', (req, res) => {
     res.send("Panda Tap backend is running correctly!");
 });
 
-const PORT = process.env.PORT || 8080;
+// User routes
+app.use('/user', userRoutes);
+
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-
 });
-
-
-app.use('/user', user);
