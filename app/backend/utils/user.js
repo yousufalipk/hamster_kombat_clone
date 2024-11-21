8bf1fcf640b485f61e8b6ae954916cd381102275
yousufalipk <yousufbhatti2002@gmail.com>
2024-11-21 13:05:01 +0500

    Changes 123

--- app/backend/utils/user.js
+++ app/backend/utils/user.js
@@ -3,10 +3,6 @@ const { TELEGRAM_BOT_TOKEN } = require('../config/env');
 
 exports.getProfilePhoto = async (telegramId) => {
     try {
-
-        console.log("telegrma ID", telegramId);
-        console.log("Bot token", TELEGRAM_BOT_TOKEN);
-
         const photosResponse = await axios.get(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/getUserProfilePhotos`, {
             params: {
                 user_id: telegramId,
@@ -16,7 +12,7 @@ exports.getProfilePhoto = async (telegramId) => {
 
         const photos = photosResponse.data.result.photos;
         if (!photos || photos.length === 0) {
-            return ({ success: false, mess: 'No profile photo found for this user' });
+            return { success: false, mess: 'No profile photo found for this user' };
         }
 
         const fileId = photos[0][0].file_id;
@@ -25,15 +21,21 @@ exports.getProfilePhoto = async (telegramId) => {
         });
 
         const filePath = fileResponse.data.result.file_path;
+        const fileSize = fileResponse.data.result.file_size;
+
+        if (fileSize > 1024 * 1024) {
+            return { success: false, mess: 'Profile photo exceeds the 1MB size limit' };
+        }
+
         const fileUrl = `https://api.telegram.org/file/bot${TELEGRAM_BOT_TOKEN}/${filePath}`;
 
         const imageResponse = await axios.get(fileUrl, { responseType: 'arraybuffer' });
         const base64Data = Buffer.from(imageResponse.data, 'binary').toString('base64');
 
-        return ({ success: true, mess: 'photo found', photo: `data:image/jpeg;base64,${base64Data}` });
+        return { success: true, mess: 'Photo found', photo: `data:image/jpeg;base64,${base64Data}` };
 
     } catch (error) {
         console.error('Error fetching or saving Telegram photo:', error.message);
-        return ({ success: false, mess: 'Internal Server Error' });
+        return { success: false, mess: 'Internal Server Error' };
     }
-}
+};
