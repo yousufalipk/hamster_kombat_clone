13ad504eb7135c42b52d3df050d11ee83e6f19a5
yousufalipk <yousufbhatti2002@gmail.com>
2024-12-13 11:36:39 +0500

    Changes 123

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,59 +1,62 @@
 import React, { useEffect } from "react";
-import { Routes, Route } from 'react-router-dom';
-import CustomLoader from './Components/Loader/Loader';
+import { Routes, Route } from "react-router-dom";
+import CustomLoader from "./Components/Loader/Loader";
 
-import Token from './Components/Token/index';
-import GamePage from './Pages/Game/Game';
+import Token from "./Components/Token/index";
+import GamePage from "./Pages/Game/Game";
 
-import Layout from './Layout/index';
+import Layout from "./Layout/index";
 
-import { useUser } from './context/index';
-
+import { useUser } from "./context/index";
 
 const App = () => {
 	const { loader, loaderErrorMes } = useUser();
 
+	// Handle Telegram close event (when user tries to close the app)
+	useEffect(() => {
+		if (window.Telegram && window.Telegram.WebApp) {
+			window.Telegram.WebApp.onEvent("close", () => {
+				const confirmation = window.confirm("Are you sure you want to close the app?");
+				if (!confirmation) {
+					// Prevent closing if the user cancels the action
+					window.Telegram.WebApp.enableClosingConfirmation();
+				}
+			});
+		}
+	}, []);
+
 	if (loader) {
 		return (
-			<>
-				<div className="h-[100vh] w-[100vw] bg-black flex justify-center items-center">
-					<CustomLoader size={200} />
-				</div>
-			</>
-		)
+			<div className="h-screen w-screen bg-black flex justify-center items-center">
+				<CustomLoader size={200} />
+			</div>
+		);
 	}
 
 	if (loaderErrorMes) {
 		return (
-			<>
-				<div className="h-[100vh] w-[100vw] bg-black flex justify-center items-center">
-					<div className="flex gap-3 justify-center items-center flex-col border-2 h-[90vh] w-[90vw] rounded-3xl">
-						<span className="text-2xl text-red-700 text-center font-bold">
-							{loaderErrorMes.mess}
-						</span>
-						<p className="text-xl text-white text-center">
-							Try reloading!
-						</p>
-					</div>
+			<div className="h-screen w-screen bg-black flex justify-center items-center">
+				<div className="flex flex-col gap-3 justify-center items-center border-2 h-[90vh] w-[90vw] rounded-3xl bg-white p-8">
+					<span className="text-2xl text-red-700 text-center font-bold">
+						{loaderErrorMes.mess}
+					</span>
+					<p className="text-xl text-white text-center">Try reloading!</p>
 				</div>
-			</>
-		)
+			</div>
+		);
 	}
 
 	return (
-		<>
-			<div className="h-screen w-screen bg-[#060611]">
-				<Routes>
-					<Route path="/" element={<Layout />} />
-					<Route path="*" element={<Layout />} />
+		<div className="h-screen w-screen bg-[#060611]">
+			<Routes>
+				<Route path="/" element={<Layout />} />
+				<Route path="*" element={<Layout />} />
 
-					{/* Other Routes */}
-					{/*<Route path="/rankings" element={<Rankings />} /> */}
-					<Route path="/gameplay" element={<GamePage />} />
-					<Route path="/token" element={<Token />} />
-				</Routes>
-			</div>
-		</>
+				{/* Other Routes */}
+				<Route path="/gameplay" element={<GamePage />} />
+				<Route path="/token" element={<Token />} />
+			</Routes>
+		</div>
 	);
 };
 
