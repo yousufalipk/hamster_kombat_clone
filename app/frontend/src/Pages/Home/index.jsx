abf9834ad793196d3fd67546dfd29fe0450f1773
yousufalipk <yousufbhatti2002@gmail.com>
2024-12-21 12:25:08 +0500

    Daily Reward Fontend Issues fixed!

--- app/frontend/src/Pages/Home/index.jsx
+++ app/frontend/src/Pages/Home/index.jsx
@@ -1646,6 +1646,13 @@ const Home = () => {
 						{dailyRewardPopup && (
 							<>
 								<div
+									onClick={() => {
+										setPopupClosing(true);
+										setTimeout(() => {
+											setDailyRewardPopup(false);
+											setPopupClosing(false);
+										}, 500);
+									}}
 									style={{
 										animation: `${popupClosing ? "fadeOut" : "fadeIn"
 											} 0.5s ease-in-out forwards`,
@@ -1659,132 +1666,149 @@ const Home = () => {
 										}}
 									>
 										<div
-											className="relative bg-[#06060E] w-[100vw] h-[80vh] rounded-t-3xl p-4 text-white">
-											<div className="absolute left-0 -top-10">
-												<img src={RightPopupEllipse} alt="" />
-											</div>
-											<div className="absolute bottom-0 right-0">
-												<img src={LeftPopupEllipse} alt="" />
-											</div>
-											<div className="absolute left-0 bottom-20">
-												<img src={leftbox} alt="" />
-											</div>
-											<div className="absolute bottom-0 right-0">
-												<img src={rightbox} alt="" />
-											</div>
-											<div className="absolute bottom-0 -inset-1 bg-[#23A7FF] rounded-[35px] -z-10"></div>
-											<div className="absolute bottom-0 -inset-2 bg-[#23A7FF] blur rounded-[50px] -z-10"></div>
-											<div className="flex flex-col gap-4 items-center justify-center z-50">
-												<p className="w-16 border-b-4 rounded-md"></p>
-												<h1 className="text-2xl font-semibold">Daily Reward</h1>
-												<p className="text-xs text-center text-[#CACACA]">
-													Earn Coins by logging in game daily! Don't miss a day, or your streak will reset!
-												</p>
-												<div className="overflow-scroll w-[90vw] h-[50vh] flex flex-wrap gap-4 justify-center items-center">
-													{days.map((day, index) => (
-														<div
-															key={index}
-															className={`border-2 relative w-[23vw] h-[14vh] border-[#AFABAB] rounded-lg pt-2 flex flex-col gap-2 ${index === 6 ? "w-[80vw]" : ""
-																} ${currentDay >= day
-																	? "bg-[#2E60B2]"
-																	: "bg-[#767676]"
-																} ${claimed.includes(day) ? "bg-[#767676]" : ""
-																}`}
-														>
-															<div>
-																<div
-																	className={`${claimed.includes(day)
-																		? "bg-[#474750]"
-																		: "bg-[#1942EC]"
-																		} mr-6 flex w-[20vw] shadow-inherit shadow-xl items-center justify-center rounded-r-md text-white`}
-																>
-																	<h1 className="font-semibold text-sm">Day {day + 1}</h1>
-																</div>
-																<div className="flex flex-col items-center mt-2 gap-1">
-																	{claimed.includes(day) ? (
-																		<div className="bg-[#D9D9D9] rounded-full h-6 w-6 flex items-center justify-center">
-																			<img
-																				src={tick}
-																				width={25}
-																				alt="tick"
-																			/>
-																		</div>
-																	) : (
-																		<>
-																			{index === 6 ? (
-																				<img
-																					className="absolute top-6"
-																					src={day7coin}
-																					width={80}
-																					alt="Big Coin"
-																				/>
-																			) : (
-																				<img
-																					src={BigCoin}
-																					width={30}
-																					alt="Big Coin"
-																				/>
-																			)}
-																		</>
-																	)}
-
-																	{index === 6 ? (
-																		<h2
-																			className={`text-md text-[#000000] absolute bottom-1 font-medium ${claimed.includes(day) &&
-																				"text-[#393838]"
-																				} ${currentDay === day &&
-																				"text-[#ffffff]"
-																				}`}
-																		>
-																			{reward[day]}
-																		</h2>
-																	) : (
-																		<h2
-																			className={`text-md text-[#000000] font-medium ${claimed.includes(day) &&
-																				"text-[#393838]"
-																				} ${currentDay === day &&
-																				"text-[#ffffff]"
-																				}`}
-																		>
-																			{reward[day]}
-																		</h2>
-																	)}
-																</div>
-															</div>
-														</div>
-													))}
+											onClick={(e) => e.stopPropagation()}
+											className="popup-content"
+										>
+											<div
+												className=" relative bg-[#06060E] w-[100vw] h-[80vh] rounded-t-3xl p-4 text-white">
+												<div className="absolute left-0 -top-10">
+													<img src={RightPopupEllipse} alt="" />
 												</div>
-											</div>
-											{/* action buttons */}
-											<div className="flex gap-4 justify-center mt-8">
-												<button
-													className="h-10 min-w-10 px-3 p-2 bg-[#242434] rounded-xl text-sm z-50 border-b-4 border-b-[#191922] shadow-zinc-900"
-													onClick={handleDailyRewardClaim}
-													disabled={claimed.includes(currentDay) || buttonLoading}
+												<div className="absolute bottom-0 right-0">
+													<img src={LeftPopupEllipse} alt="" />
+												</div>
+												<div className="absolute left-0 bottom-20">
+													<img src={leftbox} alt="" />
+												</div>
+												<div className="absolute bottom-0 right-0">
+													<img src={rightbox} alt="" />
+												</div>
+												<div
+													className="popup-main flex flex-col"
 												>
-													{buttonLoading ? (
-														<span className="h-6 font-bold">
-															{dots}
-														</span>
-													) : (
-														claimed.includes(currentDay) ? (
-															<span>Come back tomorrow</span>
-														) : (
-															<span>Claim</span>
-														)
-													)}
-												</button>
-											</div>
-											<div className="absolute top-4 right-5">
-												<button onClick={() => {
-													setPopupClosing(true);
-													setTimeout(() => {
-														setDailyRewardPopup(false);
-														setPopupClosing(false);
-													}, 500);
-												}}>
-													<img src={CrossImg} alt="" width={25} />
-												</button>
+													<div className="absolute bottom-0 -inset-1 bg-[#23A7FF] rounded-[35px] -z-10"></div>
+													<div className="absolute bottom-0 -inset-2 bg-[#23A7FF] blur rounded-[50px] -z-10"></div>
+
+													<div
+														style={{
+															zIndex: 1000
+														}}
+														className="absolute top-4 right-4">
+														<button onClick={() => {
+															setPopupClosing(true);
+															setTimeout(() => {
+																setDailyRewardPopup(false);
+																setPopupClosing(false);
+															}, 500);
+														}}>
+															<img src={CrossImg} alt="" width={25} />
+														</button>
+													</div>
+
+													<div className="flex flex-col gap-4 items-center justify-center z-50">
+														<p className="w-16 border-b-4 rounded-md"></p>
+														<h1 className="text-2xl font-semibold">Daily Reward</h1>
+														<p className="text-xs text-center text-[#CACACA]">
+															Earn Coins by logging in game daily! Don't miss a day, or your streak will reset!
+														</p>
+														<div className="overflow-scroll w-[90vw] h-[50vh] flex flex-wrap gap-4 justify-center items-center">
+															{days.map((day, index) => (
+																<div
+																	key={index}
+																	className={`border-2 relative w-[23vw] h-[14vh] border-[#AFABAB] rounded-lg pt-2 flex flex-col gap-2 ${index === 6 ? "w-[80vw]" : ""
+																		} ${currentDay >= day
+																			? "bg-[#2E60B2]"
+																			: "bg-[#767676]"
+																		} ${claimed.includes(day) ? "bg-[#767676]" : ""
+																		}`}
+																>
+																	<div>
+																		<div
+																			className={`${claimed.includes(day)
+																				? "bg-[#474750]"
+																				: "bg-[#1942EC]"
+																				} mr-6 flex w-[20vw] shadow-inherit shadow-xl items-center justify-center rounded-r-md text-white`}
+																		>
+																			<h1 className="font-semibold text-sm">Day {day + 1}</h1>
+																		</div>
+																		<div className="flex flex-col items-center mt-2 gap-1">
+																			{claimed.includes(day) ? (
+																				<div className="bg-[#D9D9D9] rounded-full h-6 w-6 flex items-center justify-center">
+																					<img
+																						src={tick}
+																						width={25}
+																						alt="tick"
+																					/>
+																				</div>
+																			) : (
+																				<>
+																					{index === 6 ? (
+																						<img
+																							className="absolute top-6"
+																							src={day7coin}
+																							width={80}
+																							alt="Big Coin"
+																						/>
+																					) : (
+																						<img
+																							src={BigCoin}
+																							width={30}
+																							alt="Big Coin"
+																						/>
+																					)}
+																				</>
+																			)}
+
+																			{index === 6 ? (
+																				<h2
+																					className={`text-md text-[#000000] absolute bottom-1 font-medium ${claimed.includes(day) &&
+																						"text-[#393838]"
+																						} ${currentDay === day &&
+																						"text-[#ffffff]"
+																						}`}
+																				>
+																					{reward[day]}
+																				</h2>
+																			) : (
+																				<h2
+																					className={`text-md text-[#000000] font-medium ${claimed.includes(day) &&
+																						"text-[#393838]"
+																						} ${currentDay === day &&
+																						"text-[#ffffff]"
+																						}`}
+																				>
+																					{reward[day]}
+																				</h2>
+																			)}
+																		</div>
+																	</div>
+																</div>
+															))}
+														</div>
+													</div>
+													{/* action buttons */}
+													<div className="flex gap-4 justify-center mt-8">
+														<button
+															className="h-10 min-w-12 px-3 p-2 bg-[#242434] rounded-xl text-sm z-50 border-b-4 border-b-[#191922] shadow-zinc-900"
+															onClick={handleDailyRewardClaim}
+															disabled={claimed.includes(currentDay) || buttonLoading}
+														>
+															{buttonLoading ? (
+																<span className="flex justify-center items-center text-5xl font-bold w-full">
+																	<p className="absolute -mt-6">
+																		{dots}
+																	</p>
+																</span>
+															) : (
+																claimed.includes(currentDay) ? (
+																	<span>Come back tomorrow</span>
+																) : (
+																	<span>Claim</span>
+																)
+															)}
+														</button>
+													</div>
+												</div>
 											</div>
 										</div>
 									</div>
