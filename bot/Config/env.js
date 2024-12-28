1c01c29143134f91676a3528d6b848bebee1369c
yousufalipk <yousufbhatti2002@gmail.com>
2024-12-28 06:20:29 +0500

    Bot text updated!

--- bot/Config/env.js
+++ bot/Config/env.js
@@ -5,9 +5,15 @@ dotenv.config();
 const PORT = process.env.PORT;
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const REACT_APP_URL = process.env.REACT_APP_URL;
+const ANNOUNCEMENT_CHANNEL_URL = process.env.ANNOUNCEMENT_CHANNEL_URL;
+const TELEGRAM_CHAT_URL = process.env.TELEGRAM_CHAT_URL;
+const GAME_BOT_URL = process.env.GAME_BOT_URL;
 
 module.exports = {
     PORT,
     BOT_TOKEN,
-    REACT_APP_URL
+    REACT_APP_URL,
+    ANNOUNCEMENT_CHANNEL_URL,
+    TELEGRAM_CHAT_URL,
+    GAME_BOT_URL
 }
--- bot/server.js
+++ bot/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { Telegraf } = require('telegraf');
 
-const { PORT, BOT_TOKEN } = require('./Config/env');
+const { PORT, BOT_TOKEN, ANNOUNCEMENT_CHANNEL_URL, TELEGRAM_CHAT_URL, GAME_BOT_URL } = require('./Config/env');
+
+console.log('Bot Token ', BOT_TOKEN);
 
 const bot = new Telegraf(BOT_TOKEN);
 
@@ -9,8 +11,37 @@ const app = express();
 
 app.use(express.json());
 
-bot.start((ctx) => ctx.reply('Welcome! How can I help you?'));
+
+bot.start((ctx) => {
+	const username = ctx.from.username || "there";
+	const welcomeMessage = `
+:tada: Welcome ${username} to PandaTap! :panda_face:
+Ready to see how high you can climb? Tap away and watch your fortunes grow!
+
+Step into the world of PandaTap and begin your journey from the ground up to the prestigious Founder level! Upgrade your cards and level up by tapping and completing various tasks within the app.
+
+And remember, adventures are best with friends.
+:handshake: Invite your friends: more friends — more profit! :money_with_wings:
+`;
+
+	const inlineButtons = [
+		{ text: ':mega: Announcement Channel', url: ANNOUNCEMENT_CHANNEL_URL },
+		{ text: ':loud_sound: Telegram Chat', url: TELEGRAM_CHAT_URL },
+		{ text: ':video_game:  Game Bot', url: GAME_BOT_URL }
+	];
+
+	const keyboard = {
+		inline_keyboard: inlineButtons.map(button => [{ text: button.text, url: button.url }])
+	};
+
+	return ctx.reply(welcomeMessage, {
+		reply_markup: keyboard
+	});
+});
+
 bot.help((ctx) => ctx.reply('Available commands: /start, /help'));
+
+
 bot.on('text', (ctx) => ctx.reply(`You said: ${ctx.message.text}`));
 
 const BOT_WEBHOOK_PATH = '/bot';
